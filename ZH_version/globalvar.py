8bf78996b392292012168d50270a125e85408692
Doublefire.Chen <1604015204@qq.com>
2022-02-03 15:51:28 +0800

    Create globalvar.py

---
+++ ZH_version/globalvar.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#全局变量管理模块，参考：https://www.jb51.net/article/130193.htm#
+global _global_dict
+_global_dict={}
+def set_value(name, value):
+ _global_dict[name]=value
+def get_value(name, defValue=None):
+ try:
+  return _global_dict[name]
+ except KeyError:
+  return defValue
